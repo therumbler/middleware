b11e67b21c8d24e344b698b693efa3945a49c6d3
caleb <yocalebo@gmail.com>
2022-06-21 16:04:10 -0400

    clean up bits.py

--- src/middlewared/middlewared/plugins/interface/netif_linux/bits.py
+++ src/middlewared/middlewared/plugins/interface/netif_linux/bits.py
@@ -1,49 +1,42 @@
-# -*- coding=utf-8 -*-
-import enum
-import logging
-
-logger = logging.getLogger(__name__)
+from enum import IntEnum
 
 __all__ = ["InterfaceCapability", "InterfaceFlags", "InterfaceLinkState", "NeighborDiscoveryFlags"]
 
 
-class InterfaceCapability(enum.IntEnum):
+class InterfaceCapability(IntEnum):
     pass
 
 
-class InterfaceFlags(enum.IntEnum):
+class InterfaceFlags(IntEnum):
     # include/uapi/linux/if.h
-
-    UP = 1 << 0
-    BROADCAST = 1 << 1
-    DEBUG = 1 << 2
-    LOOPBACK = 1 << 3
-    POINTOPOINT = 1 << 4
-    # DRV_RUNNING = defs.IFF_DRV_RUNNING
-    NOARP = 1 << 7
-    PROMISC = 1 << 8
-    ALLMULTI = 1 << 9
-    # DRV_OACTIVE = defs.IFF_DRV_OACTIVE
-    # SIMPLEX = defs.IFF_SIMPLEX
-    # LINK0 = defs.IFF_LINK0
-    # LINK1 = defs.IFF_LINK1
-    # LINK2 = defs.IFF_LINK2
-    MULTICAST = 1 << 12
-    # CANTCONFIG = defs.IFF_CANTCONFIG
-    # PPROMISC = defs.IFF_PPROMISC
-    # MONITOR = defs.IFF_MONITOR
-    # STATICARP = defs.IFF_STATICARP
-    # DYING = defs.IFF_DYING
-    # RENAMING = defs.IFF_RENAMING
+    UP = 1 << 0  # sysfs
+    BROADCAST = 1 << 1  # volatile
+    DEBUG = 1 << 2  # sysfs
+    LOOPBACK = 1 << 3  # volatile
+    POINTOPOINT = 1 << 4  # volatile
+    NOTRAILERS = 1 << 5  # sysfs
+    RUNNING = 1 << 6  # volatile
+    NOARP = 1 << 7  # sysfs
+    PROMISC = 1 << 8  # sysfs
+    ALLMULTI = 1 << 9  # sysfs
+    MASTER = 1 << 10  # volatile
+    SLAVE = 1 << 11  # volatile
+    MULTICAST = 1 << 12  # sysfs
+    PORTSEL = 1 << 13  # sysfs
+    AUTOMEDIA = 1 << 14  # sysfs
+    DYNAMIC = 1 << 15  # sysfs
+    LOWER_UP = 1 << 16
+    DORMANT = 1 << 17
+    ECHO = 1 << 18
 
 
-class InterfaceLinkState(enum.IntEnum):
+class InterfaceLinkState(IntEnum):
     LINK_STATE_UNKNOWN = 0
     LINK_STATE_DOWN = 1
     LINK_STATE_UP = 2
 
 
-class NeighborDiscoveryFlags(enum.IntEnum):
+class NeighborDiscoveryFlags(IntEnum):
     PERFORMNUD = 0
     ACCEPT_RTADV = 0
     PREFER_SOURCE = 0
